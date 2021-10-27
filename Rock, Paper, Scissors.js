5c3487ec8f69cf15d8fa37f641e51b16cbd85173
Rafa <rafa@Rafas-MacBook-Pro.local>
2021-10-27 14:22:16 -0700

    Fix the logic with the condition BOMB

--- Rock, Paper, Scissors.js
+++ Rock, Paper, Scissors.js
@@ -3,28 +3,33 @@ const getUserChoice = (userInput) => {
   if (
     userInput === "rock" ||
     userInput === "paper" ||
-    userInput === "scissors"
+    userInput === "scissors" ||
+    userInput === "bomb"
   ) {
     return userInput;
   } else {
-    return "Please, chose between rock, paper, or scissors.";
-  }
+    return "Please, choose between rock, paper, or scissors."
+  }  
 };
 
 const getComputerChoice = () => {
-  let math = Math.floor(Math.random() * 3);
+  let math = Math.floor(Math.random() * 3)
 
   switch (math) {
     case 0:
       return "rock";
     case 1:
-      return "paper";ß
+      return "paper";
     case 2:
       return "scissors";
   }
-};
+}
 
 const determineWinner = (userChoice, computerChoice) => {
+  if(userChoice === 'bomb') {
+    return "The user won!"
+  }
+  
   if (userChoice === computerChoice) {
     return "The game is tied";
   }
@@ -35,31 +40,38 @@ const determineWinner = (userChoice, computerChoice) => {
       return "You won!";
     }
   }
-  if (userChoice === "paper") {
-    if (computerChoice === "scissors") {
-      return "The computer won!";
-    } else {
-      return "You won!";
-    }
-    if (userChoice === "scissors") {
-      if (computerChoice === "rock") {
+    if (userChoice === "paper") {
+      if (computerChoice === "scissors") {
         return "The computer won!";
       } else {
         return "You won!";
       }
+      if (userChoice === "scissors") {
+        if (computerChoice === "rock") {
+          return "The computer won!";
+        } else {
+          return "You won!";
+        }
+      }
+      
     }
-    if (useChoice === "bomb") {
-      return "Congratulation you won the opportunity to follow the Empire D.C at https://www.facebook.com/empiredcofficial";
-    }
-  }
-};
+  };
 
-const playGame = () => {
-  const userChoice = getUserChoice("paper");
-  const computerChoice = getComputerChoice();
-  console.log("The user choose: " + userChoice);
-  console.log("The computer choose: " + computerChoice);
-  console.log(determineWinner(userChoice, computerChoice));
-};
+  const playGame = () =>{
+    const userChoice = getUserChoice("bomb");
+    const computerChoice = getComputerChoice();
+    console.log('The user choose: ' + userChoice)
+    console.log('The computer choose: ' + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+  };
 
-playGame();
+ playGame();
+
+
+
+
+
+
+
+
+
